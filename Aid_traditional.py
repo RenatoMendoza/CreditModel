e885334c04aaff675a89769bd9375e0bc80eb8bf
Frizard03 <frizard03@gmail.com>
2025-04-06 14:11:31 -0600

    tests

--- Aid_traditional.py
+++ Aid_traditional.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import traditional_borrador as tb
+
 
 def clean_data_trad(file_name):
     df = pd.read_csv(file_name)
